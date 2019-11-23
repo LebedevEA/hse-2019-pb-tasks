ac92163eeb208c2a3fd8d15bc363a72298b8be8a
Lebedev Egor <lebedev.3gor@gmail.com>
2019-11-23 05:42:43 +0300

    Add old version to GitHub

--- task01-grep/grep.py
+++ task01-grep/grep.py
@@ -1,29 +1,77 @@
 #!/usr/bin/env python3
 from typing import List
+from typing import IO
 import sys
 import re
 import argparse
 
 
-def main(args_str: List[str]):
+def parse_args(args_str: List[str]) -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument('needle', type=str)
     parser.add_argument('files', nargs='*')
     parser.add_argument('-E', dest='regex', action='store_true')
-    args = parser.parse_args(args_str)
+    parser.add_argument('-c', dest='count', action='store_true')
+    return parser.parse_args(args_str)
 
-    # STUB BEGINS
-    for line in sys.stdin.readlines():
+
+def main(args_str: List[str]) -> None:
+    args = parse_args(args_str)
+
+    if not args.regex:
+        args.needle = re.escape(args.needle)
+
+    if not args.files:
+        lines_in_stdin(args.needle, args.count)
+    else:
+        lines_in_files(args.needle, args.count, args.files)
+
+
+def save_matching_lines(pattern: str, input_place: IO[str], count: bool) -> List[str]:
+    lines: List[str] = []
+    for line in input_place.readlines():
+        lines.append(line)
+    lines = [str(len(save_line(lines, pattern)))] if count else save_line(lines, pattern)
+    return lines
+
+
+def lines_in_stdin(pattern: str, count: bool) -> None:
+    lines = save_matching_lines(pattern, sys.stdin, count)
+    print_single_input(lines)
+
+
+def lines_in_files(pattern: str, count: bool, files: List[str]) -> None:
+    for file in files:
+        with open(file, 'r') as in_file:
+            lines = save_matching_lines(pattern, in_file, count)
+            print_line_in_file(lines, file, bool(len(files) - 1))
+
+
+def save_line(lines: List[str], pattern: str) -> List[str]:
+    return_list = []
+    for line in lines:
         line = line.rstrip('\n')
-        if args.needle in line:
-            print('Found needle in ' + line)
+        if re.search(pattern, line):
+            return_list.append(line)
+    return return_list
 
-    with open('input.txt', 'r') as in_file:
-        for line in in_file.readlines():
-            line = line.rstrip('\n')
-            if re.search(args.needle, line):
-                print(f'Found re in {line}')
-    # STUB ENDS
+
+def print_single_input(lines: List[str]) -> None:
+    for line in lines:
+        print(line)
+
+
+def print_line_in_file(lines: List[str], file: str, if_many_files: bool) -> None:
+    if if_many_files:
+        print_in_many_inputs(file, lines)
+        lines.clear()
+    else:
+        print_single_input(lines)
+
+
+def print_in_many_inputs(file: str, lines: List[str]) -> None:
+    for line in lines:
+        print(file + ':' + line)
 
 
 if __name__ == '__main__':
--- task01-grep/test_grep.py
+++ task01-grep/test_grep.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import io
+import re
 import grep
 
 
@@ -57,3 +58,250 @@ def test_integrate_files_grep_count(tmp_path, monkeypatch, capsys):
     out, err = capsys.readouterr()
     assert err == ''
     assert out == 'b.txt:1\na.txt:2\n'
+
+
+def test_integrate_print_in_many_inputs_1(capsys):
+    list_ = ['Line_1', 'Line_2', 'Line_3', 'Line_4']
+    grep.print_in_many_inputs('file_name', list_)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'file_name:Line_1\nfile_name:Line_2\nfile_name:Line_3\nfile_name:Line_4\n'
+
+
+def test_integrate_print_in_many_inputs_2(capsys):
+    list_ = ['10']
+    grep.print_in_many_inputs('file_name', list_)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'file_name:10\n'
+
+
+def test_integrate_print_in_many_inputs_3(capsys):
+    list_ = []
+    grep.print_in_many_inputs('file_name', list_)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == ''
+
+
+def test_integrate_print_single_input_1(capsys):
+    list_ = ['Line_1', 'Line_2', 'Line_3', 'Line_4']
+    grep.print_single_input(list_)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'Line_1\nLine_2\nLine_3\nLine_4\n'
+
+
+def test_integrate_print_single_input_2(capsys):
+    list_ = ['61261242851875618374653453245862345235']
+    grep.print_single_input(list_)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '61261242851875618374653453245862345235\n'
+
+
+def test_integrate_print_line_in_file_1(capsys):
+    list_ = ['Line_1', 'Line_2', 'Line_3', 'Line_4']
+    grep.print_line_in_file(list_, 'file_name', 1)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'file_name:Line_1\nfile_name:Line_2\nfile_name:Line_3\nfile_name:Line_4\n'
+    assert len(list_) == 0
+
+
+def test_integrate_print_line_in_file_2(capsys):
+    list_ = ['Line_1', 'Line_2', 'Line_3', 'Line_4']
+    grep.print_line_in_file(list_, 'file_name', 0)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'Line_1\nLine_2\nLine_3\nLine_4\n'
+
+
+def test_integrate_print_line_in_file_3(capsys):
+    list_ = ['10']
+    grep.print_line_in_file(list_, 'file_name', 1)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'file_name:10\n'
+    assert len(list_) == 0
+
+
+def test_integrate_print_line_in_file_4(capsys):
+    list_ = ['10']
+    grep.print_line_in_file(list_, 'file_name', 0)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '10\n'
+
+
+def test_integrate_save_line_1():
+    line = 'example of line with\n'
+    lines = []
+    pattern = re.escape('exam')
+    lines = grep.save_line(line, pattern)
+    assert len(lines) == 1
+    assert lines[0] == 'example of line with'
+
+
+def test_integrate_save_line_2():
+    line = 'example of line with\n'
+    lines = []
+    pattern = re.escape('ex.m')
+    lines = grep.save_line(line, pattern)
+    assert len(lines) == 0
+
+
+def test_integrate_save_line_3():
+    line = 'example of line with\n'
+    pattern = 'ex.m'
+    lines = grep.save_line(line, pattern)
+    # assert len(lines) == 1
+    assert lines[0] == 'example of line with'
+
+
+def test_integrate_save_line_4():
+    line = 'example of line with\n'
+    lines = []
+    pattern = 'exami.ate'
+    lines = grep.save_line(line, pattern)
+    assert len(lines) == 0
+
+
+def test_integrate_lines_in_files_1(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('needless\nuseless')
+    files = ['a.txt']
+    monkeypatch.chdir(tmp_path)
+    pattern = re.escape('needle')
+    grep.lines_in_files(pattern, 0, files)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'needless\n'
+
+
+def test_integrate_lines_in_files_2(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('needless\nuseless')
+    files = ['a.txt']
+    monkeypatch.chdir(tmp_path)
+    pattern = re.escape('needle')
+    grep.lines_in_files(pattern, 1, files)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '1\n'
+
+
+def test_integrate_lines_in_files_3(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('needless\nuseless')
+    files = ['a.txt']
+    monkeypatch.chdir(tmp_path)
+    pattern = 'ne.dle'
+    grep.lines_in_files(pattern, 0, files)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'needless\n'
+
+
+def test_integrate_lines_in_files_4(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('needless\nuseless')
+    files = ['a.txt']
+    monkeypatch.chdir(tmp_path)
+    pattern = 'needle.s'
+    grep.lines_in_files(pattern, 1, files)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == '1\n'
+
+
+def test_integrate_lines_in_files_5(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('needless\nuseless')
+    (tmp_path / 'b.txt').write_text('needle noodle\ndoodle')
+    files = ['a.txt', 'b.txt']
+    monkeypatch.chdir(tmp_path)
+    pattern = re.escape('needle')
+    grep.lines_in_files(pattern, 0, files)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:needless\nb.txt:needle noodle\n'
+
+
+def test_integrate_lines_in_files_6(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('needless\nuseless')
+    (tmp_path / 'b.txt').write_text('needle noodle\ndoodle')
+    files = ['a.txt', 'b.txt']
+    monkeypatch.chdir(tmp_path)
+    pattern = re.escape('needle')
+    grep.lines_in_files(pattern, 1, files)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:1\nb.txt:1\n'
+
+
+def test_integrate_lines_in_files_7(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('needless\nuseless')
+    (tmp_path / 'b.txt').write_text('lorem ipsum')
+    files = ['a.txt', 'b.txt']
+    monkeypatch.chdir(tmp_path)
+    pattern = re.escape('needle')
+    grep.lines_in_files(pattern, 0, files)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:needless\n'
+
+
+def test_integrate_lines_in_files_8(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('needless\nuseless')
+    (tmp_path / 'b.txt').write_text('dolor sin amet')
+    files = ['a.txt', 'b.txt']
+    monkeypatch.chdir(tmp_path)
+    pattern = re.escape('needle')
+    grep.lines_in_files(pattern, 1, files)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:1\nb.txt:0\n'
+
+
+def test_integrate_lines_in_files_9(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('needless\nuseless')
+    (tmp_path / 'b.txt').write_text('needle noodle\ndoodle')
+    files = ['a.txt', 'b.txt']
+    monkeypatch.chdir(tmp_path)
+    pattern = 'n.ed.e'
+    grep.lines_in_files(pattern, 0, files)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:needless\nb.txt:needle noodle\n'
+
+
+def test_integrate_lines_in_files_10(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('needless\nuseless')
+    (tmp_path / 'b.txt').write_text('needle noodle\ndoodle')
+    files = ['a.txt', 'b.txt']
+    monkeypatch.chdir(tmp_path)
+    pattern = 'n.ed.e'
+    grep.lines_in_files(pattern, 1, files)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:1\nb.txt:1\n'
+
+
+def test_integrate_lines_in_files_11(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('needless\nuseless')
+    (tmp_path / 'b.txt').write_text('lorem ipsum')
+    files = ['a.txt', 'b.txt']
+    monkeypatch.chdir(tmp_path)
+    pattern = 'n.ed.e'
+    grep.lines_in_files(pattern, 0, files)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:needless\n'
+
+
+def test_integrate_lines_in_files_12(tmp_path, monkeypatch, capsys):
+    (tmp_path / 'a.txt').write_text('needless\nuseless')
+    (tmp_path / 'b.txt').write_text('dolor sin amet')
+    files = ['a.txt', 'b.txt']
+    monkeypatch.chdir(tmp_path)
+    pattern = 'n.ed.e'
+    grep.lines_in_files(pattern, 1, files)
+    out, err = capsys.readouterr()
+    assert err == ''
+    assert out == 'a.txt:1\nb.txt:0\n'
